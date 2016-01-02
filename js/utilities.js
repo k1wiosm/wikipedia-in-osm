fafe412af534f12270f4c1527cb52b33c142b6da
k1wiosm <k1wiosm@gmail.com>
2016-01-02 15:07:12 +0100

    Add wikipedia link

--- js/utilities.js
+++ js/utilities.js
@@ -62,6 +62,8 @@ function showData (data) {
 		if (data.elements[i].tags && data.elements[i].tags.wikipedia) {
 			var lat = data.elements[i].lat || data.elements[i].center.lat;
 			var lon = data.elements[i].lon || data.elements[i].center.lon;
+			var lang = data.elements[i].tags.wikipedia.split(':')[0];
+			var article = data.elements[i].tags.wikipedia.split(':')[1];
 			var marker = L.circleMarker([lat, lon]).addTo(markers)
 				.bindPopup(''+
 					'<a href="http://openstreetmap.org/'+
@@ -69,7 +71,8 @@ function showData (data) {
 					data.elements[i].id+'">'+
 					data.elements[i].type+
 					data.elements[i].id+'</a></br>'+
-					data.elements[i].tags.wikipedia+' '
+					'<a href="http://'+lang+'.wikipedia.org/wiki/'+
+					article+'">'+data.elements[i].tags.wikipedia+'</a>'
 					);
 			switch (data.elements[i].type) {
 				case 'node': marker.setStyle({color:'blue'}); break;
