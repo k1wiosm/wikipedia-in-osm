6dca6bb318c2e16bcf63e147686857fab97a328b
k1wi <k1wiosm@gmail.com>
2017-07-14 17:30:37 +0200

    add support for wikidata

--- js/utilities.js
+++ js/utilities.js
@@ -46,6 +46,9 @@ function loadData () {
 		map.getBounds().getEast()+')';
 	var query=''+
 		'('+
+			'node["wikidata"]'+bbox+';'+
+			'way["wikidata"]'+bbox+';'+
+			'relation["wikidata"]'+bbox+';'+
 			'node["wikipedia"]'+bbox+';'+
 			'way["wikipedia"]'+bbox+';'+
 			'relation["wikipedia"]'+bbox+';'+
@@ -59,21 +62,35 @@ function showData (data) {
 	console.log(data);
 	markers.clearLayers();
 	for (var i = 0; i < data.elements.length; i++) {
-		if (data.elements[i].tags && data.elements[i].tags.wikipedia) {
+		if (data.elements[i].tags) {
 			var lat = data.elements[i].lat || data.elements[i].center.lat;
 			var lon = data.elements[i].lon || data.elements[i].center.lon;
-			var lang = data.elements[i].tags.wikipedia.split(':')[0];
-			var article = data.elements[i].tags.wikipedia.split(':')[1];
+			var wikipedia = null;
+			var wikidata = null;
+			if (data.elements[i].tags.wikipedia) {
+				var wikipedia = {};
+				wikipedia.lang = data.elements[i].tags.wikipedia.split(':')[0];
+				wikipedia.title = data.elements[i].tags.wikipedia.split(':')[1];
+			}
+			if (data.elements[i].tags.wikidata) {
+				var wikidata = data.elements[i].tags.wikidata;
+			}
+			popup = 		'<a href="https://openstreetmap.org/'+
+							data.elements[i].type+'/'+
+							data.elements[i].id+'">'+
+							data.elements[i].type+
+							data.elements[i].id+'</a></br>';
+			if (wikidata) {
+				popup += 	'<a href="https://www.wikidata.org/wiki/'+
+							wikidata+'">'+wikidata+'</a></br>';
+			}
+			if (wikipedia) {
+				popup +=	'<a href="http://'+wikipedia.lang+
+							'.wikipedia.org/wiki/'+wikipedia.title+'">'+
+							wikipedia.lang+":"+wikipedia.title+'</a>';
+			}
 			var marker = L.circleMarker([lat, lon]).addTo(markers)
-				.bindPopup(''+
-					'<a href="http://openstreetmap.org/'+
-					data.elements[i].type+'/'+
-					data.elements[i].id+'">'+
-					data.elements[i].type+
-					data.elements[i].id+'</a></br>'+
-					'<a href="http://'+lang+'.wikipedia.org/wiki/'+
-					article+'">'+data.elements[i].tags.wikipedia+'</a>'
-					);
+				.bindPopup(popup);
 			switch (data.elements[i].type) {
 				case 'node': marker.setStyle({color:'blue'}); break;
 				case 'way': marker.setStyle({color:'red'}); break;
